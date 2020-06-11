1b596020a7b7c4881eac0497442f091d0af3230a
elipri <Ely@Kasutajas-MacBook-Pro.local>
2020-06-11 17:06:51 +0300

    Bf adding new page; added icons, tested functions

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor() {}
+  public todos: Array<{title:string, description:string}>=[];
+  constructor() {
+    this.todos.push(
+      {title:'Esimene todo', description:'See on esimene todo.'},
+      {title:'Teine todo', description:'See on teine todo.'},
+      {title:'Kolmas todo', description:'See on kolmas todo.'}
+    )
+  }
+  
+  addTodo() {
+    console.log('blah');
+  }
+  editTodo() {
+    console.log('blah-blah');
+  }
 
 }
