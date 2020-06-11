4de975a6ac8405eab39f4497e45cad0ae558d81f
elipri <Ely@Kasutajas-MacBook-Pro.local>
2020-06-11 23:58:45 +0300

    Added storage and reconfigured app

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 //impordin loodud klassi Task ülesannete loomiseks; asub models kataloogis
 import { Task } from '../models/task';
 
@@ -9,29 +10,56 @@ import { Task } from '../models/task';
 export class DataService {
   //public todos: Array <{title:string, description:string}> = [];
   public todos: Array<Task> = [];
-  constructor() { }
+  constructor(private storage: Storage) { }
 
   load() {
     //console.log('loading data');
-    this.todos.push(
+    //promise käivitab asünk tegevuse
+    this.storage.get('todos').then((data) => {
+      if(data != null) {
+        this.todos = data;
+      } else {
+        this.todos = [];
+      }
+    });
+    /* this.todos.push(
       { id:1, title: 'Esimene todo', description: 'See on esimene todo'},
       { id:2, title: 'Teine todo', description: 'See on teine todo'},
       { id:3, title: 'Kolmas todo', description: 'See on kolmas todo'}
-    );
+    ); */
   }
 
   getIndex(id: number): number {
-    return this.todos.findIndex(x => x.id === id);
+    if (this.todos != null) {
+      return this.todos.findIndex(x => x.id === id);
+    }
+    return -1; //if not found
   }
+
   get(id: number): Task {
     return this.todos.filter(x=> x.id === id)[0];
+  }
+
+  getMaxId(): number {
+    if (this.todos != null) {
+      return Math.max.apply(Math, this.todos.map(o => o.id));
+    }
+    return -1;
   }
 
   save(todo: Task) {
     //console.log('saving data');
     const index = this.todos.indexOf(todo);
-    if (todo != null) {
+    if (todo !== null) {
+      //id ID has not yet been generated, assign it
+      if(todo.id === 0 || todo.id == null) {
+        const newId = this.getMaxId() + 1;
+        todo.id = newId;
+        console.log('Assigning new ID ' + newId);
+      }
+
       const index = this.getIndex(todo.id);
+
       if (index === -1) {
         console.log('Adding');
         this.todos.push(todo);
@@ -39,6 +67,8 @@ export class DataService {
         console.log('Updating at index ' + index);
         this.todos[index] = todo;
       }
+
+      this.storage.set('todos', this.todos);
     }
   }
 }
